5d620f549a34ed8e96c495514bdcdd7eef771a02
Harsh12chauhan <harshchauhan0225@gmail.com>
2022-07-06 10:48:52 +0530

    Day 2 at reactnative bootcamp 1

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { React } from "react";
+import {  Text, ScrollView, Button,StyleSheet } from "react-native";
+import  Login  from "./components/Login";
+import Signup  from "./components/Signup";
 
-export default function App() {
+const App = () => {
+
+const name= "harsh";
+const age =" 20";
+
+  function onclick() {
+    alert("HELLO")
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView >
+      <Login name1={name} age1={age}/>
+      <Text style={{ fontSize: 50,color:'red',backgroundColor:'black' }}>My App</Text>
+      <Text style={{ fontSize: 50 }}>Hello world</Text>
+      <Text style={{ fontSize: 50 }}>My App</Text>
+      <Text style={[styles.try1, styles.try2]}>Try once </Text>
+      <Signup/>
+      <Button title="Click"  onPress={onclick} color="#1E6738"/>
+    </ScrollView>
   );
 }
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  try1:{
+    color:'red',
+    fontSize:100
   },
-});
+  try2:{
+    backgroundColor:'black',
+  }
+})
+
---
+++ components/Signup.js
@@ -0,0 +1,12 @@
+// import { React } from "react";
+import {Text, ScrollView } from "react-native";
+
+const Signup = () => {
+  return (
+    <ScrollView>
+      <Text style={{fontSize:50}} >Signup Page</Text>
+      <Text style={{ color: "green" }} >itsgreen</Text>
+    </ScrollView>
+  );
+};
+export default Signup;
