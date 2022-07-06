c7e80d14c4bf2deab8f3266fcfde0f3d97c3d3d2
Harsh12chauhan <harshchauhan0225@gmail.com>
2022-07-06 12:52:29 +0530

    how to use image and uri's

--- App.js
+++ App.js
@@ -1,38 +1,90 @@
 import { React } from "react";
-import {  Text, ScrollView, Button,StyleSheet } from "react-native";
-import  Login  from "./components/Login";
-import Signup  from "./components/Signup";
+import {
+  View,
+  Text,
+  ScrollView,
+  Button,
+  Image,
+  ImageBackground,
+} from "react-native";
+// import Login from "./components/Login";
+// import Signup from "./components/Signup";
 
 const App = () => {
-
-const name= "harsh";
-const age =" 20";
-
-  function onclick() {
-    alert("HELLO")
+  function onClick() {
+    alert("Hello");
   }
-
   return (
-    <ScrollView >
-      <Login name1={name} age1={age}/>
-      <Text style={{ fontSize: 50,color:'red',backgroundColor:'black' }}>My App</Text>
-      <Text style={{ fontSize: 50 }}>Hello world</Text>
-      <Text style={{ fontSize: 50 }}>My App</Text>
-      <Text style={[styles.try1, styles.try2]}>Try once </Text>
-      <Signup/>
-      <Button title="Click"  onPress={onclick} color="#1E6738"/>
+    <ScrollView>
+      <View style={{ flex: 1 }}>
+        <View
+          style={{
+            flex: 1,
+            height: 100,
+            margin: 40,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <ImageBackground
+            source={{ url: "https://i.stack.imgur.com/1dpmw.gif" }}
+            style={{flex:1,  justifyContent:"center", alignItems:"center"}}
+          >
+            <Text style={{fontSize: 50}}>Hello</Text>
+          </ImageBackground>
+        </View>
+        <View
+          style={{
+            flex: 4,
+            backgroundColor: "purple",
+            height: 100,
+            margin: 30,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Image
+            source={require("./Image/img.png")}
+            style={{ height: 400, resizeMode: "contain" }}
+          ></Image>
+
+          <Text style={{ color: "white" }}>Purple Color</Text>
+        </View>
+        <View
+          style={{
+            flex: 6,
+            backgroundColor: "red",
+            height: 100,
+            margin: 30,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ color: "white" }}>Red Color</Text>
+          <Image
+            source={{ url: "https://i.stack.imgur.com/1dpmw.gif" }}
+            style={{ height: 100, resizeMode: "contain" }}
+          ></Image>
+        </View>
+        <View
+          style={{
+            flex: 7,
+            backgroundColor: "black",
+            height: 100,
+            margin: 30,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Image
+            source={require("./Image/img.gif")}
+            style={{ height: 120, resizeMode: "contain" }}
+          ></Image>
+          <Text style={{ color: "white" }}> Black color</Text>
+        </View>
+      </View>
+      <Button title="Click" onPress={onClick} color="#1E6738" />
     </ScrollView>
   );
-}
+};
 export default App;
-
-const styles = StyleSheet.create({
-  try1:{
-    color:'red',
-    fontSize:100
-  },
-  try2:{
-    backgroundColor:'black',
-  }
-})
-
